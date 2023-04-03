2ed5f37f8d09c19ef8b22dd03fc62c1d7458fa91
ivanrakonjac <rakonjacivan98@gmail.com>
2023-04-03 15:27:18 +0200

    JwtAuthenticationFilter created

---
+++ Spring Security/src/main/java/com/jwt/security/configuration/JwtAuthenticationFilter.java
@@ -0,0 +1,24 @@
+package com.jwt.security.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
