8c8557cafdf7d4ef03114a2dfe287c17a3b5ac13
ivanrakonjac <rakonjacivan98@gmail.com>
2023-04-03 14:46:29 +0200

    UserEntity added

---
+++ Spring Security/src/main/java/com/jwt/security/entities/UserEntity.java
@@ -0,0 +1,36 @@
+package com.jwt.security.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user_entity")
+public class UserEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+}
