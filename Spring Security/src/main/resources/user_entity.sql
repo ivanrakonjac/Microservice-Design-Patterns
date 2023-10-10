affa67d2902918f6f187f3589461cc312060f087
ivanrakonjac <rakonjacivan98@gmail.com>
2023-10-10 14:03:00 +0200

    user_entity.sql script added

---
+++ Spring Security/src/main/resources/user_entity.sql
@@ -0,0 +1,8 @@
+CREATE TABLE public.user_entity (
+                                    username varchar NULL,
+                                    first_name varchar NULL,
+                                    last_name varchar NULL,
+                                    email varchar NULL,
+                                    "password" varchar NULL,
+                                    "role" varchar NULL
+);
