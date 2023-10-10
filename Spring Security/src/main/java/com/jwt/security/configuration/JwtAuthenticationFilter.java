a584d134af9d344971ad2687b7d8409fe8508687
ivanrakonjac <rakonjacivan98@gmail.com>
2023-10-10 14:03:33 +0200

    doFilterInternal method changed in JwtAuthenticationFilter

--- Spring Security/src/main/java/com/jwt/security/configuration/JwtAuthenticationFilter.java
+++ Spring Security/src/main/java/com/jwt/security/configuration/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.jwt.security.configuration;
 
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -37,35 +38,41 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
 
         final String jwt = authHeader.substring(7);
-        final String username = jwtService.extractUsername(jwt);
+        final String username;
 
-        // SecurityContextHolder.getContext().getAuthentication() == null means that user is not authenticated
-        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-
-            // We are checking user from the DB
-            // UserDetailsService bean is created in ApplicationConfig file where we implemented loadUserByUsername method
-            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-
-            if(jwtService.isTokenValid(jwt, userDetails)){
-
-                // Class needed by SpringSecurity to updated UserDetails info
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                        userDetails,
-                        null,
-                        userDetails.getAuthorities()
-                );
-
-                // Method to save some additional details about the authentication request, such as the remote IP address and the session ID
-                authToken.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(request)
-                );
-
-                SecurityContextHolder.getContext().setAuthentication(authToken);
-            }
-
-            // I am passing request to filter chain
-            filterChain.doFilter(request, response);
+        try{
+            username = jwtService.extractUsername(jwt);
+        }catch (JwtException e){
+            response.sendError(400);
+            return;
         }
 
+        // SecurityContextHolder.getContext().getAuthentication() == null means that user is not authenticated
+        if(username == null || SecurityContextHolder.getContext().getAuthentication() != null) return;
+
+        // We are checking user from the DB
+        // UserDetailsService bean is created in ApplicationConfig file where we implemented loadUserByUsername method
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        if(jwtService.isTokenValid(jwt, userDetails)){
+
+            // Class needed by SpringSecurity to updated UserDetails info
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    null,
+                    userDetails.getAuthorities()
+            );
+
+            // Method to save some additional details about the authentication request, such as the remote IP address and the session ID
+            authToken.setDetails(
+                    new WebAuthenticationDetailsSource().buildDetails(request)
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+        }
+
+        // I am passing request to filter chain
+        filterChain.doFilter(request, response);
     }
+
 }
