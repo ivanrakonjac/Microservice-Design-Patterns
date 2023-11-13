d6ddfc6c4ff4f4f6aea7d629b9c0125286686d93
ivanrakonjac <rakonjacivan98@gmail.com>
2023-11-13 12:38:20 +0100

    Dto validation added to AuthenticationController

--- Spring Security/src/main/java/com/jwt/security/auth/AuthenticationController.java
+++ Spring Security/src/main/java/com/jwt/security/auth/AuthenticationController.java
@@ -5,6 +5,7 @@ import com.jwt.security.auth.dtos.AuthenticationResponse;
 import com.jwt.security.auth.dtos.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +19,12 @@ public class AuthenticationController {
     private final AuthService authService;
 
     @PostMapping(value = "/register")
-    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody @Validated RegisterRequest request) {
         return ResponseEntity.ok(authService.register(request));
     }
 
     @PostMapping(value = "/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody @Validated AuthenticationRequest request) {
         return ResponseEntity.ok(authService.authenticate(request));
     }
 
